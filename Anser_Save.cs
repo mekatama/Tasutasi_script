a7ec9cc3859c334757850ae016588b2402c4cc70
mekatama <tamatte@hotmail.com>
2017-10-06 17:18:28 +0900

    UIスライダー値の取得と保存

---
+++ Anser_Save.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Anser_Save : MonoBehaviour {
+	public float anserNum;	//
+	public Slider slider;	//Sliderコンポーネント取得用
+	public Text text;		//Textコンポーネント取得用
+
+	public void SaveNum () {
+		anserNum = slider.value;		//スライド値を取得
+		text.text = anserNum.ToString("000");
+//		Debug.Log("Test" + anserNum);	//スライド値取得Log		
+	}
+}
