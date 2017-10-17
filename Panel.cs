f80145640b36b6465e05e52422c0d8674eb66b6c
mekatama <tamatte@hotmail.com>
2017-10-17 18:20:30 +0900

    数字panel用スクリプト
    数字の設定
    GameControllerに数字を渡す
    GameControllerに削除数を渡す

---
+++ Panel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour {
+	public int panelNum;				//panelの番号を入れる
+	public GameObject gameController;	//GameController取得
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");	//GameControllerオブジェクトを探す
+	}
+
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.tag == "DestroyArea"){
+			//gcって仮の変数にGameControllerのコンポーネントを入れる
+			GameController gc = gameController.GetComponent<GameController>();
+
+			//【予定】gamrControllerにpanelNumを渡して計算できそうか実験する
+			if(gc.panelNum[0] == 0){
+				gc.panelNum[0] = panelNum;
+			}else if(gc.panelNum[1] == 0){
+				gc.panelNum[1] = panelNum;
+			}
+
+			//削除したパネル数カウント
+			gc.panelDestroyNum ++;
+			//このGameObjectを［Hierrchy］ビューから削除する
+			Destroy(gameObject);
+		}
+	}
+}
