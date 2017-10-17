17c1011c5c5a0ba00ce391e2ddc744c999a11edf
mekatama <tamatte@hotmail.com>
2017-10-17 18:18:32 +0900

    未使用になりました

---
+++ UI_Start.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//不要になりました
+public class UI_Start : MonoBehaviour {
+	void Start () {
+//		Destroy(gameObject,2.0f);	//オブジェクトの削除
+	}
+}
