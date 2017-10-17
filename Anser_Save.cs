eb14982395603aa279f0ebb189227aece7d8b18b
mekatama <tamatte@hotmail.com>
2017-10-17 18:21:12 +0900

    成否判定を入れる

--- Anser_Save.cs
+++ Anser_Save.cs
@@ -7,10 +7,22 @@ public class Anser_Save : MonoBehaviour {
 	public float anserNum;	//
 	public Slider slider;	//Sliderコンポーネント取得用
 	public Text text;		//Textコンポーネント取得用
+	public GameObject gameController;	//GameController取得
 
 	public void SaveNum () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+
 		anserNum = slider.value;		//スライド値を取得
 		text.text = anserNum.ToString("000");
-//		Debug.Log("Test" + anserNum);	//スライド値取得Log		
+
+		//計算の合計と比較
+		if(gc.panelNumTotal == anserNum){
+			//正解
+			Debug.Log("Good Job !!");
+		}else{
+			//間違い
+			Debug.Log("Miss !!");
+		}
 	}
 }
