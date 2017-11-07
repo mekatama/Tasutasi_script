8fd9331a466306e2fbc15d35881e8e566e5421f5
mekatama <tamatte@hotmail.com>
2017-11-07 18:20:24 +0900

    コメント整理

--- UI_Slider.cs
+++ UI_Slider.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_Slider : MonoBehaviour {
-	private Slider slider;	//Sliderコンポーネント取得用
-	private float level;	//スライド値を入れる用
-	public Text slideNumText;		//Textコンポーネント取得用
+	private Slider slider;		//Sliderコンポーネント取得用
+	private float level;		//スライド値を入れる用
+	public Text slideNumText;	//Textコンポーネント取得用
 
 
 	void Start () {
@@ -19,17 +19,15 @@ public class UI_Slider : MonoBehaviour {
 		//スライド値が変化したら
 		if (slider.value != level) {
 			level = slider.value;		//スライド値の変化判定用に再設定
-			Debug.Log(slider.value);	//スライド値取得Log
-
+//			Debug.Log(slider.value);	//スライド値取得Log
 			slideNumText.text = slider.value.ToString("000");
-			
 		}
 	}
 
 	//Sliderの値が変わる度にスクリプトのメソッドを呼び出す
     public void MoveSlider(){
 		//ここ、使用するかどうかは未定
-        Debug.Log("Moving");
+//		Debug.Log("Moving");
     }
 
 }
