16e7ba7e2ca63ed14364d0f615f5f33585b0adc5
mekatama <tamatte@hotmail.com>
2017-11-23 17:21:30 +0900

    〇✖表示の制御

--- GameController.cs
+++ GameController.cs
@@ -7,7 +7,8 @@ public class GameController : MonoBehaviour {
 	enum State{
 		Ready,
 		Play,
-		Anser
+		Anser,
+		Result
 	}
 
 	State state;
@@ -16,16 +17,23 @@ public class GameController : MonoBehaviour {
 	public int panelNumTotal;	//合計
 	public int panelDestroyNum;	//生成後に削除したpanel数
 	public bool isAnser;		//解答したフラグ
+	public bool isSeikai;		//正解フラグ
 
 	public float startTime = 1.5f;	//UIのSTARTを表示する時間
 	float time = 0f;				//UIのSTARTを表示する時間用の変数
+	public float resultTime = 1.5f;	//UIの〇✖を表示する時間
+	float time_marubatu = 0f;		//UIの〇✖を表示する時間用の変数
 
 	public Canvas startCamvas;		//Canvas_Start
 	public Canvas inputCamvas;		//Canvas_Input
+	public Canvas maruCamvas;		//Canvas_Maru
+	public Canvas batuCamvas;		//Canvas_Batu
 	public PanelSpawn ps;			//PanelSpawn1
 
 	void Start () {
 		inputCamvas.enabled = false;	//InputUI非表示
+		maruCamvas.enabled = false;		//maruUI非表示
+		batuCamvas.enabled = false;		//batuUI非表示
 		calcNum = 2;					//【仮】後からスタート時に選択予定
 		Ready();						//ステート変更
 		time = 0;						//UIのSTARTを表示する時間用の変数の初期化
@@ -59,13 +67,34 @@ public class GameController : MonoBehaviour {
 				Debug.Log("State.Anser");
 				//解答したら、以下を呼び出せば良さそう
 				if(isAnser == true){
-					inputCamvas.enabled = false;	//InputUI非表示
+					Result();	//ステート変更
+				}
+				break;
+			//〇✖表示
+			case State.Result:
+				//ここで〇✖の表示をする
+				if(isSeikai){
+					maruCamvas.enabled = true;		//maruUI表示
+				}else{
+					batuCamvas.enabled = true;		//batuUI表示
+				}
+				inputCamvas.enabled = false;		//InputUI非表示
+
+				//指定時間で〇✖を非表示にする
+				time_marubatu += Time.deltaTime;
+				if(time_marubatu > resultTime){
+					maruCamvas.enabled = false;		//maruUI非表示
+					batuCamvas.enabled = false;		//batuUI非表示
+
+					//イロイロ初期化
 					panelDestroyNum = 0;			//panel削除数を初期化
 					ps.panelTotalNum = 0;			//panelの総数を初期化
 					panelNum[0] = 0;				//panelの数字を初期化
 					panelNum[1] = 0;				//panelの数字を初期化
+					time_marubatu = 0;				//〇✖表示時間を初期化
 					GameStart();	//ステート変更
 				}
+				Debug.Log("State.Result");
 				break;
 		}
 	}
@@ -89,5 +118,9 @@ public class GameController : MonoBehaviour {
 		Debug.Log("Goukei = " + panelNumTotal);
 
 		//ここで、最後の集計用に正誤を保存するかもしれない
+	}
+
+	void Result(){
+		state = State.Result;
 	}
 }
