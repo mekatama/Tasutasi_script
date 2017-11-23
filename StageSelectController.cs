356fe6eb7634e489c7618729d74fb3ce9087761f
mekatama <tamatte@hotmail.com>
2017-11-23 17:19:01 +0900

    ステージセレクト画面のボタン制御

---
+++ StageSelectController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageSelectController : MonoBehaviour {
+
+	//ステージ用の制御関数
+	public void OnStage01ButtonClicked(){
+		SceneManager.LoadScene("Main1");	//シーンのロード
+	}
+
+	//タイトルに戻るボタン用の制御関数
+	public void OnReturnTitleButtonClicked(){
+		SceneManager.LoadScene("Title");	//シーンのロード
+	}
+}
