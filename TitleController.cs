80c266a4d50dd15a90ac9eadb7010822e8ce3d79
mekatama <tamatte@hotmail.com>
2017-11-23 17:18:48 +0900

    タイトル画面のボタン制御

---
+++ TitleController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleController : MonoBehaviour {
+
+	void Update(){
+		//backキー
+		if (Input.GetKeyUp(KeyCode.Escape)){
+			Application.Quit();	//アプリ終了
+		}
+	}
+
+	//セレクト画面用の制御関数
+	public void OnSelectButtonClicked(){
+		SceneManager.LoadScene("Select");	//シーンのロード
+	}
+
+	//遊び方ボタン用の制御関数
+	public void OnHowToPlayButtonClicked(){
+		SceneManager.LoadScene("HowToPlay");	//シーンのロード
+	}
+
+	//タイトルに戻るボタン用の制御関数
+//	public void OnReturnTitleButtonClicked(){
+//		SceneManager.LoadScene("Title");	//シーンのロード
+//	}
+	
+	//アプリ終了
+	public void OnExitButtonClicked(){
+		Application.Quit();
+		Debug.Log("exit");	
+	}
+}
