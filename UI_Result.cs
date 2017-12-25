3af8fa194f437a0e7eb3babe114f7ac99e550548
mekatama <tamatte@hotmail.com>
2017-12-25 16:22:23 +0900

    リザルト画面UI用

---
+++ UI_Result.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Result : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text resultText;				//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//結果テキスト表示
+		resultText.text = gc.syutudaiNum + "問中　" + gc.seikaiNum + "問正解";
+	}
+}
