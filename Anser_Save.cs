76ac70124f89958a0474b98997b5226a2528efb7
mekatama <tamatte@hotmail.com>
2017-12-25 16:23:06 +0900

    最後の集計用に正誤を保存

--- Anser_Save.cs
+++ Anser_Save.cs
@@ -16,14 +16,22 @@ public class Anser_Save : MonoBehaviour {
 		anserNum = slider.value;		//スライド値を取得
 		text.text = anserNum.ToString("000");
 
+		//ここで、最後の集計用に正誤を保存するかもしれない
+		int i = gc.syutudaiNumNow - 1;
+
 		//計算の合計と比較
 		if(gc.panelNumTotal == anserNum){
 			//正解
+			gc.seigo[i] = 1;				//配列に正解1を入れておく
+			Debug.Log("配列番号[" + i + "]  = (正解)" + gc.seigo[i]);
 //			Debug.Log("Good Job !!");
+			gc.seikaiNum += 1;
 			gc.isSeikai = true;
 			gc.isAnser = true;
 		}else{
 			//間違い
+			gc.seigo[i] = 2;				//配列に不正解2を入れておく
+			Debug.Log("配列番号[" + i + "]  = (不正解)" + gc.seigo[i]);
 //			Debug.Log("Miss !!");
 			gc.isSeikai = false;
 			gc.isAnser = true;
