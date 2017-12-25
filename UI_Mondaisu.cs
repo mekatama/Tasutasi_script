edc7ff7a0c31895b46114b399a5c6071d948753e
mekatama <tamatte@hotmail.com>
2017-12-25 16:22:40 +0900

    スタート画面UI用

---
+++ UI_Mondaisu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Mondaisu : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text resultText;				//Textコンポーネント取得用
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//結果テキスト表示
+		resultText.text = "全　" + gc.syutudaiNum + "　問";	}
+}
