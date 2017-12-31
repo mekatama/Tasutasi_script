ad942ca7587571d5bea9ac80a4feb3d698f95c2b
mekatama <tamatte@hotmail.com>
2017-12-31 18:46:45 +0900

    何問目の入力かを伝えるUI

---
+++ UI_Nanmonme.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Nanmonme : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text nanmonmeText;			//Textコンポーネント取得用
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//テキスト表示
+		nanmonmeText.text = "第　" + gc.syutudaiNumNow + "　問";
+	}
+}
