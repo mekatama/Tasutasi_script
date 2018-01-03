4a9748d1beb6f1e5c65816f9ad4b50dfc199800b
mekatama <tamatte@hotmail.com>
2018-01-03 22:34:36 +0900

    数字と絵柄panelを出せるように

--- PanelSpawn.cs
+++ PanelSpawn.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PanelSpawn : MonoBehaviour {
 	public GameObject[] panelObject;	//panelのプレハブを配列で管理
+	public GameObject[] panelObject_i;	//panel(image)のプレハブを配列で管理
 	public GameObject gameController;	//GameController取得
 	public bool panelSpawn = false;		//pane出現フラグ
 	public float timeOut;				//panelを出現させたい時間間隔
 	private float timeElapsed;			//時間を仮に格納する変数
 	private int panelType;				//panelの種類
+	private int panelImageType;			//panelの絵柄種類
 	public int panelTotalNum;			//panelの総数
 	private int spawnType;				//生成位置の種類(上0 左1 右2)
+	private GameObject kariPaneru;		//仮代入用
 
 	void Start () {
 	}
@@ -33,6 +36,7 @@ public class PanelSpawn : MonoBehaviour {
 	public void PanelGo(){
 		float x_pos = Random.Range(-2.0f, 2.0f);	//ランダムで出現位置を決める
 		float y_pos = Random.Range(0.0f, 5.0f);		//ランダムで出現位置を決める
+		panelImageType = Random.Range(0, 2);		//ランダムで出現panelの絵柄を選出
 		panelType = Random.Range(0, 2);				//ランダムで出現panelを選出
 		spawnType = Random.Range(0, 3);				//ランダムで出現する位置を選出
 		timeOut = 1.5f;								//【仮】出現させたい時間間隔
@@ -51,8 +55,16 @@ public class PanelSpawn : MonoBehaviour {
 		}
 
 		//panelを生成する
+		//まず、数字か絵柄をセレクト
+		//仮、ランダムかセレクトか選べるようにしたい
+		if(panelImageType == 0){
+			kariPaneru = panelObject[panelType];
+		}else{
+			kariPaneru = panelObject_i[panelType];
+		}
+
 		GameObject panel = (GameObject)Instantiate(
-			panelObject[panelType],		//生成するpanel
+			kariPaneru,					//生成するpanel
 			new Vector2(x_pos, y_pos),	//生成時の位置
 			transform.rotation			//生成時の角度
 		);
