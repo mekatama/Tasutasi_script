56bda4cdef41c47af3309c390a418fee6b9e4243
mekatama <tamatte@hotmail.com>
2018-02-02 17:02:56 +0900

    紙吹雪エフェクトを消す処理

---
+++ AutoDestroyEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyEffect : MonoBehaviour {
+	ParticleSystem particle;
+
+	void Start () {
+		particle = GetComponent<ParticleSystem>();
+	}
+	
+	void Update () {
+		if(particle.isPlaying == false){
+			Destroy(gameObject);
+		}
+	}
+}
