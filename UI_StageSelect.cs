1df075f0a8fb1e8d13fe7aac4a0b652027c512eb
mekatama <tamatte@hotmail.com>
2018-02-15 18:54:02 +0900

    セレクト画面でハイスコア表示準備

---
+++ UI_StageSelect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_StageSelect : MonoBehaviour {
+	public Text highScore1;		//ハイスコアを表示するオブジェクト用
+
+	void Update () {
+		//ハイスコア系テキストの表示
+		highScore1.text = PlayerPrefs.GetFloat("HighScore1").ToString("000.000") + "s";
+	}
+}
