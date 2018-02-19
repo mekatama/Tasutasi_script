056dddadb4a87460a9702d202047132e5d5f0157
mekatama <tamatte@hotmail.com>
2018-02-19 17:32:23 +0900

    リザルト画面にハイスコア表示

---
+++ UI_HighScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HighScore : MonoBehaviour {
+//	public GameObject gameController;	//GameController取得
+	public Text highScoreText;			//Textコンポーネント取得用
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+//		GameController gc = gameController.GetComponent<GameController>();
+		//time表示
+		highScoreText.text = PlayerPrefs.GetFloat("HighScore1").ToString("000.000");
+	}
+}
