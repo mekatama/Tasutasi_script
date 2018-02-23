dca80d841ba68dbcbc3642b9b64bc81560dcfa93
mekatama <tamatte@hotmail.com>
2018-02-23 18:59:20 +0900

    ハイスコア変数の取り方変更

--- UI_HighScore.cs
+++ UI_HighScore.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UI_HighScore : MonoBehaviour {
-//	public GameObject gameController;	//GameController取得
+	public GameObject gameController;	//GameController取得
 	public Text highScoreText;			//Textコンポーネント取得用
 	
 	void Update () {
 		//gcって仮の変数にGameControllerのコンポーネントを入れる
-//		GameController gc = gameController.GetComponent<GameController>();
+		GameController gc = gameController.GetComponent<GameController>();
 		//time表示
-		highScoreText.text = PlayerPrefs.GetFloat("HighScore1").ToString("000.000");
+//		highScoreText.text = PlayerPrefs.GetFloat("HighScore1").ToString("000.000");
+		highScoreText.text = gc.timeBest.ToString("000.000");
 	}
 }
