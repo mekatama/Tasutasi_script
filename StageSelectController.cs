255284c2dc2a06dcaaa91833d4e3f565dca58075
mekatama <tamatte@hotmail.com>
2018-03-16 16:03:49 +0900

    ステージオープンにUI対応

--- StageSelectController.cs
+++ StageSelectController.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class StageSelectController : MonoBehaviour {
+	public Canvas stageButton2;		//Canvas_s2
+	public Canvas stageButton3;		//Canvas_s3
+	public Canvas stageButton4;		//Canvas_s4
+	public Canvas stageButton5;		//Canvas_s5
+	public Canvas stageButton6;		//Canvas_s6
+	public Canvas stageButton7;		//Canvas_s7
+	public Canvas stageButton8;		//Canvas_s8
+	public Canvas stageButton9;		//Canvas_s9
+	public Canvas stageButton10;	//Canvas_s10
+	public Canvas stageButton11;	//Canvas_s11
+	public Canvas stageButton12;	//Canvas_s12
 
 	void Start () {
 		//ハイスコアの初期値チェック
@@ -22,6 +33,42 @@ public class StageSelectController : MonoBehaviour {
 		if(PlayerPrefs.GetFloat("HighScore11") == 0f){	PlayerPrefs.SetFloat("HighScore11", 120.0f);}
 		if(PlayerPrefs.GetFloat("HighScore12") == 0f){	PlayerPrefs.SetFloat("HighScore12", 120.0f);}
 //		Debug.Log("HighScore1:" + PlayerPrefs.GetFloat("HighScore1"));
+
+		stageButton2.enabled = false;		//UI表示
+		stageButton3.enabled = false;		//UI表示
+		stageButton4.enabled = false;		//UI表示
+		stageButton5.enabled = false;		//UI表示
+		stageButton6.enabled = false;		//UI表示
+		stageButton7.enabled = false;		//UI表示
+		stageButton8.enabled = false;		//UI表示
+		stageButton9.enabled = false;		//UI表示
+		stageButton10.enabled = false;		//UI表示
+		stageButton11.enabled = false;		//UI表示
+		stageButton12.enabled = false;		//UI表示
+		//■残りのボタンを非表示
+
+		//ボタン表示制御の初期値
+		if(PlayerPrefs.GetInt("StageOpen") == 0){	PlayerPrefs.SetInt("StageOpen", 1);}
+
+		//■２以上の時の処理を入れ,UIを表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 2){	stageButton2.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 3){	stageButton3.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 4){	stageButton4.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 5){	stageButton5.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 6){	stageButton6.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 7){	stageButton7.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 8){	stageButton8.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 9){	stageButton9.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 10){	stageButton10.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 11){	stageButton11.enabled = true;	}	//UI表示
+		if(PlayerPrefs.GetInt("StageOpen") >= 12){	stageButton12.enabled = true;	}	//UI表示
+
+		//StageOpenの保存処理を考える
+		
+	}
+
+	void Update () {
+
 	}
 
 	//ステージ用の制御関数
